8b761994048f8114e4c9f3a722b9c16364428bc2
EliasRubin <33438216+EliasRubin@users.noreply.github.com>
2020-01-04 20:22:24 -0800

    Update Sort.py

--- Sort.py
+++ Sort.py
@@ -2,7 +2,44 @@ import json
 import sys
 import os
 
-
+# n is size of heap 
+def heapify(arr, n, i): 
+    largest = i  # Initialize largest as root 
+    l = 2 * i + 1     # left = 2*i + 1 
+    r = 2 * i + 2     # right = 2*i + 2 
+  
+    # See if left child of root exists and is 
+    # greater than root 
+    if l < n and arr[i] < arr[l]: 
+        largest = l 
+  
+    # See if right child of root exists and is 
+    # greater than root 
+    if r < n and arr[largest] < arr[r]: 
+        largest = r 
+  
+    # Change root, if needed 
+    if largest != i: 
+        arr[i],arr[largest] = arr[largest],arr[i]  # swap 
+  
+        # Heapify the root. 
+        heapify(arr, n, largest) 
+  
+# The main function to sort an array of given size 
+def sort(list): 
+    n = len(list) 
+  
+    # Build a maxheap. 
+    for i in range(n, -1, -1): 
+        heapify(list, n, i) 
+  
+    # One by one extract elements 
+    for i in range(n-1, 0, -1): 
+        list[i], list[0] = list[0], list[i]   # swap 
+        heapify(list, i, 0) 
+	
+	
+	
 #makes sure the propper arguments are inputed
 if len(sys.argv) != 2:
 	print('Error: Invalid Input')
@@ -13,20 +50,30 @@ try:
         
 	#opens a given file
         with open(sys.argv[1], "r") as file:
-        	data = file.readlines()
+        	list = file.readlines()
             	
-		sortedList = []
-            
+		
+		
 		#Sort into sortedList here
+		
+		
+		
+		sort(list)
+		
+		
+		
         	for line in data:
+			#This section inserts the first two values into the list to work with
+        		
 			
+           	
                 
 
 		file.close()
                     
 		
 		#prints out the sorted list of words
-        	for x in sortedList:
+        	for x in listist:
 			print(x)     
             
 
